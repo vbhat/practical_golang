ed59129efb8efadf49193cddcc9a5444f49005f2
Vipul Bhat <vbhat@enova.com>
2020-01-02 08:49:12 -0600

    Add for loop

--- web_app.go
+++ web_app.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type SiteMapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
 	resp.Body.Close()
+
+	var s SiteMapIndex
+	xml.Unmarshal(bytes, &s)
+
+	for _, loc := range s.Locations {
+		fmt.Println(loc)
+	}
+
 }
